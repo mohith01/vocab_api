4b77991236e48238fe48a52d2887b270d69ef418
memehunter <mohith01@gmail.com>
2021-06-26 10:12:00 +0530

    Commit 4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.22'
 DESCRIPTION = 'vocab_api'
 LONG_DESCRIPTION = 'A package with vocabulary.com api.'
 
@@ -16,7 +16,7 @@ setup(
     name="vocab_api",
     version=VERSION,
     author="mohith01",
-    author_email="<mohith01@neuralnine.com>",
+    author_email="mohith01@gmail.com",
     description=DESCRIPTION,
     packages=find_packages(),
     install_requires=['requests','bs4'],
